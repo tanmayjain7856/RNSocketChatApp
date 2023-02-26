c18173ac00b297a2061bb55b834d07e5009a70fa
tanmayjain7856 <tanmayjain987@gmail.com>
2023-02-26 13:57:30 +0530

    Add text input to enter chat messages

--- ChatApp/source/screens/ChatScreen/ChatScreen.tsx
+++ ChatApp/source/screens/ChatScreen/ChatScreen.tsx
@@ -1,16 +1,25 @@
-import React, {useEffect} from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, TextInput} from 'react-native';
 import io from 'socket.io-client';
 import styles from '../../styles/style';
 
 export default function ChatScreen() {
+  const [messageToSend, setMessageToSend] = useState('');
+
   useEffect(() => {
     io('http://192.168.1.4:3002');
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Hello React Native!</Text>
+      <Text style={styles.textStyle}>Hello React Native!</Text>
+      <TextInput
+        value={messageToSend}
+        placeholder="Enter chat message..."
+        placeholderTextColor="#999"
+        onChangeText={(text: any) => setMessageToSend(text)}
+        style={styles.textInputStyle}
+      />
     </View>
   );
 }
--- ChatApp/source/styles/style.ts
+++ ChatApp/source/styles/style.ts
@@ -7,6 +7,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#FFF',
   },
+  textStyle: {
+    color: '#000',
+    fontSize: 16,
+    fontWeight: '700',
+  },
+  textInputStyle: {
+    color: '#000',
+  },
 });
 
 export default styles;
