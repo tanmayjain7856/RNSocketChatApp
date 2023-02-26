799e8ae5108e8e08f0cfce4f98fced6077690493
tanmayjain7856 <tanmayjain987@gmail.com>
2023-02-26 16:32:11 +0530

    Display received messages from server in Gifted Chat

--- ChatApp/source/screens/ChatScreen/ChatScreen.tsx
+++ ChatApp/source/screens/ChatScreen/ChatScreen.tsx
@@ -3,7 +3,6 @@ import {GiftedChat} from 'react-native-gifted-chat';
 import io from 'socket.io-client';
 
 export default function ChatScreen() {
-  // const [messageToSend, setMessageToSend] = useState('');
   const [recvMessagesFromServer, setRecvMessagesFromServer] = useState<any>([]);
 
   const socket = useRef<any>(null);
@@ -11,7 +10,22 @@ export default function ChatScreen() {
   useEffect(() => {
     socket.current = io('http://192.168.1.4:3002');
     socket.current.on('message', (message: any) => {
-      setRecvMessagesFromServer((prevState: any) => [...prevState, message]);
+      const testMessage = [
+        {
+          _id: 3,
+          text: 'Hello from server',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ];
+      testMessage[0].text = message;
+      setRecvMessagesFromServer((prevState: any) =>
+        GiftedChat.append(prevState, testMessage),
+      );
     });
     setRecvMessagesFromServer([
       {
@@ -24,13 +38,28 @@ export default function ChatScreen() {
           avatar: 'https://placeimg.com/140/140/any',
         },
       },
+      {
+        _id: 2,
+        text: 'Hello from client',
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
     ]);
   }, []);
 
-  // function sendMessageToServer() {
-  //   socket.current.emit('message', messageToSend);
-  //   setMessageToSend('');
-  // }
+  function sendMessageToServer(messages: any) {
+    socket.current.emit('message', messages[0].text);
+  }
 
-  return <GiftedChat messages={recvMessagesFromServer} user={{_id: 1}} />;
+  return (
+    <GiftedChat
+      messages={recvMessagesFromServer}
+      user={{_id: 1}}
+      onSend={messages => sendMessageToServer(messages)}
+    />
+  );
 }
